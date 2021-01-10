2a77093368ab5303ab197db701cd0463b1d0c9ff
William Wong <compulim@hotmail.com>
2021-01-09 19:46:15 -0800

    Revert sample

--- samples/express-server.js
+++ samples/express-server.js
@@ -1,7 +1,6 @@
 const { DefaultAzureCredential } = require('@azure/identity');
 const app = require('express')();
-// const createACMEMiddleware = require('acme-http-01-azure-key-vault-middleware/express');
-const createACMEMiddleware = require('../express');
+const createACMEMiddleware = require('acme-http-01-azure-key-vault-middleware/express');
 
 const { PORT = 80 } = process.env;
 
